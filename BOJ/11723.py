4b28e9ef71ae4da3fc5072dd4d41d3fb13fce85b
wonseok22 <84431483+wonseok22@users.noreply.github.com>
2021-06-29 14:32:24 +0900

    Update 11723.py

--- BOJ/11723.py
+++ BOJ/11723.py
@@ -3,5 +3,28 @@ input = sys.stdin.readline
 
 
 if __name__ == "__main__":
+    result = set()
     for _ in range(int(input())):
-        
+        quest = input().strip().split()
+
+        if len(quest) == 1:
+            if quest[0] == 'empty':
+                result = set()
+            else:
+                result = set(i for i in range(1, 21))
+        else:
+            que,val = quest
+            val = int(val)
+            if que == 'add':
+                result.add(val)
+            elif que == 'check':
+                print(1 if int(quest[1]) in result else 0)
+            elif que == 'remove':
+                result.discard(val)
+            else:
+                if val in result:
+                    result.discard(val)
+                else:
+                    result.add(val)
+
+

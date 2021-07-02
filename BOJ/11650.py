d3086b6b5568c98d1f155a39ebb78fc7da944af5
wonseok22 <84431483+wonseok22@users.noreply.github.com>
2021-07-02 22:49:14 +0900

    Update 11650.py

--- BOJ/11650.py
+++ BOJ/11650.py
@@ -1,6 +1,7 @@
 import sys
 input = sys.stdin.readline
 
+
 if __name__ == "__main__":
     N = int(input())
     for x,y in sorted([list(map(int,input().split())) for _ in range(N)],key = lambda x : (x[1],x[0])):

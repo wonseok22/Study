8ff501ed833bcc041ff6e59d24d7e2f4e58786bf
wonseok22 <84431483+wonseok22@users.noreply.github.com>
2021-07-12 23:03:35 +0900

    Create 11727.py

---
+++ BOJ/11727.py
@@ -0,0 +1,9 @@
+import sys
+input = sys.stdin.readline
+
+if __name__ == "__main__":
+    N = int(input())
+    dp = [0,1,3]
+    for i in range(3,N+1):
+        dp.append(dp[i-1] + dp[i-2] * 2)
+    print(dp[N]%10007)

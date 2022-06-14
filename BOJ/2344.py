6b3b25e9a0d28450ac6af7fe17af850081b0df38
wonseok22 <wonsoongnim@naver.com>
2022-06-14 11:18:22 +0900

    2022-06-14

--- BOJ/2344.py
+++ BOJ/2344.py
@@ -1,5 +1,4 @@
 import sys
-from collections import deque
 input = sys.stdin.readline
 
 dx = [-1,0,1,0]
@@ -7,7 +6,7 @@ dy = [0,1,0,-1]
 dir = [1,0,3,2]
 
 
-def out(x,y):
+def out(x,y): # 빛이 나가는 좌표에 따른 번호
     if x == -1:
         return 2*N + 2*M -y
     elif y == -1:
@@ -17,10 +16,11 @@ def out(x,y):
     else:
         return 2*N + M - x
 
-def bfs(start,direction):
+
+def search(start,direction):
     x,y = start
     while 0 <= x < N and 0 <= y < M:
-        if board[x][y] == 1:
+        if board[x][y] == 1:  #거울이 있다면 빛의 진행경로 변경
             direction = dir[direction]
         x += dx[direction]
         y += dy[direction]
@@ -30,17 +30,17 @@ N,M = map(int,input().split())
 board = [list(map(int,input().split())) for _ in range(N)]
 answer = []
 
-
+# 1번부터 2N + 2M번까지 입력 순서대로 실행
 for i in range(N):
     direction = 1
-    answer.append(bfs([i,0],direction))
+    answer.append(search([i,0],direction))
 for i in range(M):
     direction = 0
-    answer.append(bfs([N-1, i], direction))
+    answer.append(search([N-1, i], direction))
 for i in range(N-1,-1,-1):
     direction = 3
-    answer.append(bfs([i, M-1], direction))
+    answer.append(search([i, M-1], direction))
 for i in range(M-1,-1,-1):
     direction = 2
-    answer.append(bfs([0, i], direction))
+    answer.append(search([0, i], direction))
 print(' '.join(answer))

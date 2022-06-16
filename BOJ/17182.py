ff45c1d15c9acb1d40ecdcb72dade1aed7fef7c1
wonseok22 <wonsoongnim@naver.com>
2022-06-16 15:42:11 +0900

    2022-06-16

--- BOJ/17182.py
+++ BOJ/17182.py
@@ -4,9 +4,23 @@ input = sys.stdin.readline
 
 N,K = map(int,input().split())
 time = [list(map(int,input().split())) for _ in range(N)]
-distance = [[float("inf") for _ in range(N)] for _ in range(N)]
+visited = [0 for _ in range(N)]
+visited[K] = 1
+answer = float("inf")
 for k in range(N):
     for i in range(N):
         for j in range(N):
             time[i][j] = min(time[i][j], time[i][k] + time[k][j])
-print(time)
+
+def find_min(curr, cost, cnt):
+    global answer
+    if N == cnt:
+        answer = min(answer, cost)
+        return
+    for i in range(N):
+        if visited[i] == 0:
+            visited[i] = 1
+            find_min(i, cost + time[curr][i], cnt+1)
+            visited[i] = 0
+find_min(K, 0, 1)
+print(answer)

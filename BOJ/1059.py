aa2b3ae6f0cfabdb7176de8214f3b293c1edb8af
wonseok22 <wonsoongnim@naver.com>
2022-07-08 10:20:01 +0900

    2022-07-08

--- BOJ/1059.py
+++ BOJ/1059.py
@@ -9,12 +9,5 @@ if N in s:
     print(answer)
     exit()
 s.sort()
-for idx,i in enumerate(s):
-    if i > N:
-        while s[idx-1] < N:
-            answer += (i-s[idx-1]-2)
-            s[idx-1] += 1
-            print(answer)
-        print(answer)
-        break
+
 

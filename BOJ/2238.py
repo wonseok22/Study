5918347bcea685389b2147ad89129bb5c7276d4f
wonseok22 <wonsoongnim@naver.com>
2022-07-09 12:22:26 +0900

    2022-07-09

--- BOJ/2238.py
+++ BOJ/2238.py
@@ -13,6 +13,8 @@ for _ in range(N):
 for i in range(10001):
     if num[i] != 0:  #최소 경매인원이 몇명인지
         m = min(num[i],m)
+
+
 for i in range(10001): # 최소 경매인원이 있는 가장 낮은 가격을 찾는다.
     if m == num[i]:
         print(p[i][0],i)

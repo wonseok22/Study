9cf98ad25ecd6419c42e0675a9db7e9fd537cf6c
한원석 <wonsoongnim@naver.com>
2023-01-12 14:43:07 +0900

    20230112

--- BOJ/PYTHON/1062.py
+++ BOJ/PYTHON/1062.py
@@ -1,24 +1,43 @@
-from itertools import combinations
 import sys
 input = sys.stdin.readline
 
+def comb(count, x, start):
+    global answer
+    if count == K:
+        tmp = 0
+        for word in words:
+            if word & x == word:
+                tmp += 1
+        answer = max(tmp, answer)
+        # print(bin(x), tmp,count)
 
-if __name__ == "__main__":
-    N,K = map(int,input().split())
-    if K < 5:
-        print(0)
-        exit()
-    K -= 5
-    l = [input().strip()[4:-4] for _ in range(N)]
-    alpha = {}
-    for i in range(97,96+27,1):
-        alpha[chr(i)] = 0
-    alpha['a'] = 1
-    alpha['n'] = 1
-    alpha['t'] = 1
-    alpha['i'] = 1
-    alpha['c'] = 1
-    
+        return
+    for i in range(start,21):
+        x |= 1 << i
+        comb(count+1, x, i+1)
+        x &= ~(1 << i)
 
+N, K = map(int,input().split())
+words = [input().strip() for _ in range(N)]
+K-=5
+if K < 0:
+    print(0)
+    exit()
 
-
+alphabet = dict()
+alphabet_cnt = 0
+for i in range(26):
+    if chr(ord("a") + i) not in ["a","n","t","i","c"]:
+        alphabet[chr(ord("a")+i)] = alphabet_cnt
+        alphabet_cnt += 1
+    else:
+        alphabet[chr(ord("a")+i)] = -1
+for idx,word in enumerate(words):
+    wordToBin = 0
+    for s in word:
+        if alphabet[s] >-1:
+            wordToBin |= (1 << alphabet[s])
+    words[idx] = wordToBin
+answer = 0
+comb(0,0,0)
+print(answer)

dd916beb2471a9daf97bfc16fa1451694b424424
한원석 <wonsoongnim@naver.com>
2023-01-12 09:21:51 +0900

    20230112

--- BOJ/PYTHON/1083.py
+++ BOJ/PYTHON/1083.py
@@ -2,19 +2,28 @@ N = int(input())
 nums = list(map(int,input().split()))
 S = int(input())
 sorted_index = 0
-while True:
-    if sorted_index == N-1 or S == 0:
-        break
-    max_value = nums.index(max(nums[:min(len(nums),S)+1]))
-    print(nums[max_value],end= " ")
-    nums[max_value] = 0
+while S > 0 and sorted_index < N:
+    # print("-------------------------")
+    # print("S =",S, "sorted_index=",sorted_index)
+    # 남은 정렬할 원소 개수
+    remain_range = N - sorted_index
+    # S랑 정렬할 개수 중 짧은거
+    max_range = min(remain_range-1, S)
+    # print("max_range = ",max_range)
+    #탐색할 수 있는 범위
+    search_nums = nums[sorted_index:sorted_index + max_range+1]
+
+    # print("search_nums = ",search_nums)
+    # 탐색할 수 있느 범위의 가장 큰 값의 인덱스 기준점 : sorted_index
+    max_index = nums.index(max(search_nums))
+
+    # print("max_index =",max_index)
+    if max_index == sorted_index:
+        sorted_index+=1
+        continue
+    S -= max_index-sorted_index
+    nums = nums[:sorted_index] +  [nums[max_index]] + nums[sorted_index:max_index] + nums[max_index+1:]
     sorted_index += 1
-    S -= max_value-sorted_index
-    print("s = ", S)
-    print(nums)
+    # print("nums = ", nums)
+print(*nums,sep=" ")
 
-
-
-for i in nums:
-    if i != 0:
-        print(i, end=" ")

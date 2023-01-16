fea21f7387ee4a23d278f145e6e95d5e1c67db6f
한원석 <wonsoongnim@naver.com>
2023-01-16 14:20:11 +0900

    202301116

--- BOJ/PYTHON/14729.py
+++ BOJ/PYTHON/14729.py
@@ -1,4 +1,17 @@
+import sys
+input = sys.stdin.readline
+
 N = int(input())
-nums = sorted([float(input()) for _ in range(N)])[:7]
-for i in nums:
-    print("%.3f"%i)
+nums = [0 for _ in range(100001)]
+for _ in range(N):
+    num = int(float(input()) * 1000)
+    nums[num] += 1
+seven = 7
+cnt = 0
+while seven > 0:
+    if nums[cnt] > 0:
+        seven -= 1
+        nums[cnt] -= 1
+        print("{:.3f}".format(cnt / 1000))
+    else:
+        cnt += 1
